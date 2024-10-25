9125eb279bac638da08070a02e015d22c924cc5d
Atzel Fabian <24020363@uadeo.mx>
2024-10-24 23:19:28 -0700

    Add files via upload

---
+++ Parcial 1 Roman Contreras/HolaMundo2/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+int main()
+{
+    printf("Hola Mundo");
+    printf("\n Hola M");
+    printf("\XHolaMundoX\tYHolaMundoY");
+    printf("\n\n\nHolaMunda\a");
+    printf("\n\"Hola Mundo\"");
+}
